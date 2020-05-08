39a84e19e22139a3a4d34289a1eddc02dc90968d
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-05-08 01:38:24 +0000

    Updated test #03

--- exercises/03-Change-Div-Background/tests.js
+++ exercises/03-Change-Div-Background/tests.js
@@ -7,73 +7,60 @@ const css = fs.readFileSync(path.resolve(__dirname, './styles.css'), 'utf8');
 
 jest.dontMock('fs');
 
-describe('All the javascript should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-        document.documentElement.innerHTML = html.toString();
-    });
-    afterEach(() => { jest.resetModules(); });
-    it('querySelector function should be called once', function () {
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
-        //console.log(document);
-        let _document = document.cloneNode(true);
 
-        document.querySelector = jest.fn((selector) => {
-            return _document.querySelector(selector);
-        });
+beforeEach(() => {
+    //here I import the HTML into the document
+    document.documentElement.innerHTML = html.toString();
+});
+afterEach(() => { jest.resetModules(); });
+it("The querySelector function should be called once (So don't delete the existing code!)", function () {
+    /*
+        Here is how to mock the alert function:
+        https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
+    */
+    //console.log(document);
+    let _document = document.cloneNode(true);
 
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./index.js");
-
-        //and I expect the alert to be already called.
-        expect(document.querySelector.mock.calls.length).toBe(1);
-        expect(document.querySelector).toHaveBeenCalledWith("#myDiv");
-
+    document.querySelector = jest.fn((selector) => {
+        return _document.querySelector(selector);
     });
 
-    it('background color must be changed', function () {
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
-        //console.log(document);
-        let _document = document.cloneNode(true);
+    //then I import the index.js (which should have the alert() call inside)
+    const file = require("./index.js");
 
-        document.querySelector = jest.fn((selector) => {
-            return _document.querySelector(selector);
-        });
-
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./index.js");
-
-        //and I expect the alert to be already called.
-        //expect(document.querySelector("#myDiv").style.background.mock.calls.length).toBe(1);
-        expect(document.querySelector("#myDiv").style.background).toBe("yellow");
-
-    });
-
-
+    //and I expect the alert to be already called.
+    expect(document.querySelector.mock.calls.length).toBe(1);
+    expect(document.querySelector).toHaveBeenCalledWith("#myDiv");
 
 });
 
+it('The background color must be yellow', function () {
+    /*
+        Here is how to mock the alert function:
+        https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
+    */
+    //console.log(document);
+    let _document = document.cloneNode(true);
 
-describe('All the html should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-        document.documentElement.innerHTML = html.toString();
+    document.querySelector = jest.fn((selector) => {
+        return _document.querySelector(selector);
     });
-    afterEach(() => { jest.resetModules(); });
 
-    it('the html code should contain a script tag', function () {
+    //then I import the index.js (which should have the alert() call inside)
+    const file = require("./index.js");
 
-        // we can read from the source code
-        expect(html.toString().indexOf(`<script src="./index.js"></script>`) > -1).toBeTruthy();
+    //and I expect the alert to be already called.
+    //expect(document.querySelector("#myDiv").style.background.mock.calls.length).toBe(1);
+    expect(document.querySelector("#myDiv").style.background).toBe("yellow");
 
-        //or use query selector to compare hoy mane scriptags do we have
-        const scripts = document.querySelectorAll("script");
-        expect(scripts.length).toBe(1);
-    });
+});
+
+it("Don't change the index.html file", function () {
+
+    // we can read from the source code
+    expect(html.toString().indexOf(`<script src="./index.js"></script>`) > -1).toBeTruthy();
+
+    //or use query selector to compare hoy mane scriptags do we have
+    const scripts = document.querySelectorAll("script");
+    expect(scripts.length).toBe(1);
 });
