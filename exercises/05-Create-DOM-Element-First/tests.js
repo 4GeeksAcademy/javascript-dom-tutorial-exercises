c0b5a2bd1751df82385aca10dc0b71c77172d8a4
Paolo Lucano <paololucanodeveloper@gmail.com>
2020-05-08 17:54:35 +0000

    updated #05

--- exercises/05-Create-DOM-Element-First/tests.js
+++ exercises/05-Create-DOM-Element-First/tests.js
@@ -7,100 +7,77 @@ const css = fs.readFileSync(path.resolve(__dirname, './styles.css'), 'utf8');
 
 jest.dontMock('fs');
 
-describe('All the javascript should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
-        document.documentElement.innerHTML = html.toString();
-    });
-    afterEach(() => { jest.resetModules(); });
 
-    /*it('the js code should contain an assignment line creating element P', function () {
-        const expected = 'document.createElement("P")';
-        // we can read from the source code
-        console.log(js.toString());
-        expect(js.toString().indexOf(expected) > -1).toBeTruthy();
-    });*/
 
-    it('the js code should contain an assignment line creating element H1', function () {
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
-        //console.log(document);
-        let _document = document.cloneNode(true);
+it('the js code should contain an assignment line creating element H1', function () {
+  document.documentElement.innerHTML = html.toString();
+  let _document = document.cloneNode(true);
 
-        document.createElement = jest.fn((selector) => {
-            return _document.createElement(selector);
-        });
-
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./index.js");
-
-        //and I expect the alert to be already called.
-        //expect(document.createElement.mock.calls.length).toBe(1);
-        expect(document.createElement).toBeCalledWith("H1");
-
-    });
-
-    it('the js code should contain an assignment line creating text node with value "Hello World"', function () {
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
-        //console.log(document);
-        let _document = document.cloneNode(true);
-
-        document.createTextNode = jest.fn((text) => {
-            return _document.createTextNode(text);
-        });
-
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./index.js");
-
-        //and I expect the alert to be already called.
-        //expect(document.createElement.mock.calls.length).toBe(1);
-        expect(document.createTextNode).toBeCalledWith("Hello World");
-
-    });
-
-    it('the js code should contain an assignment line creating element P with background yellow', function () {
-        /*
-            Here is how to mock the alert function:
-            https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
-        */
-        //console.log(document);
-        let _document = document.cloneNode(true);
-
-        document.createElement = jest.fn((selector) => {
-            return _document.createElement(selector);
-        });
-
-        //then I import the index.js (which should have the alert() call inside)
-        const file = require("./index.js");
-
-        //and I expect the alert to be already called.
-        //expect(document.createElement.mock.calls.length).toBe(1);
-        expect(document.createElement).toBeCalledWith("P");
-        expect(document.querySelector("P").style.background).toBe("yellow");
-    });
-    /*it('the js code should contain an assignment line adding element P at the bottom of document', function () {
-        const expected = 'elembd.appendChild';
-        // we can read from the source code
-        console.log(js.toString());
-        expect(js.toString().indexOf(expected) > -1).toBeTruthy();
-    });*/
+  document.querySelector = jest.fn((selector) => {
+    if(selector === "H1")return true
+    else return false
+  });
+  expect(document.querySelector("H1")).toBeTruthy();
 });
 
+it('the js code should contain an assignment line creating text node with value "Hello World"', function () {
+    document.documentElement.innerHTML = html.toString();
+    
+    let _document = document.cloneNode(true);
 
-describe('All the html should match', function () {
-    beforeEach(() => {
-        //here I import the HTML into the document
+        document.querySelector = jest.fn((selector) => {
+    return _document.querySelector(selector);
+});
+
+    //then I import the index.js (which should have the alert() call inside)
+    const file = require("./index.js");
+
+    //and I expect the alert to be already called.
+    //expect(document.createElement.mock.calls.length).toBe(1);
+    expect(document.querySelector("H1").innerHTML).toBe("Hello World");
+
+});
+
+it('You should create a new "P" element', function () {
+    document.documentElement.innerHTML = html.toString();
+    /*
+        Here is how to mock the alert function:
+        https://stackoverflow.com/questions/41885841/how-to-mock-the-javascript-window-object-using-jest
+    */
+    //console.log(document);
+    let _document = document.cloneNode(true);
+
+    document.querySelector = jest.fn((selector) => {
+    if(selector === "P")return true
+    else return false
+  });
+  expect(document.querySelector("P")).toBeTruthy()
+
+});
+  
+it('The new "P" element should contain "Hello World" and a "yellow" background.', function () {
         document.documentElement.innerHTML = html.toString();
-    });
-    afterEach(() => { jest.resetModules(); });
+        
+        let _document = document.cloneNode(true);
+
+        document.querySelector = jest.fn((selector) => {
+          if(selector === "P")return true
+          else return false
+        });
+
+        if(expect(document.querySelector("P")).toBeTruthy()){
+          document.querySelector = jest.fn((selector) => {
+            return _document.querySelector(selector);
+          expect(document.querySelector("P").innerHTML).toBe("Hello World");
+          expect(document.querySelector("P").style.background).toBe("yellow");
+        });
+        } 
+  });
+
+
 
     it('the html code should contain a script tag', function () {
-
+        document.documentElement.innerHTML = html.toString();
         // we can read from the source code
         //console.log(html.toString());
         expect(html.toString().indexOf(`<script src="./index.js"></script>`) > -1).toBeTruthy();
@@ -109,4 +86,3 @@ describe('All the html should match', function () {
         const scripts = document.querySelectorAll("script");
         expect(scripts.length).toBe(1);
     });
-});
