e2adc5e392969483b9554071f1a97cf175cb482f
Alejandro Sanchez <aalejo@gmail.com>
2020-11-23 18:20:32 +0000

    cleaned resets and bc.json

--- .breathecode/resets/06-Create-DOM-Element-Second/index.js
+++
@@ -1,3 +0,0 @@
-//you code here
-document.body.innerHTML = "<h1>Hello World</h1>";
-
--- .breathecode/resets/07-Create-DOM-list-of-li/index.js
+++
@@ -1,6 +0,0 @@
-let beginning = "<ul>";
-let liststring = "";
-let ending = "</ul>";
-
-// do not modify after this line
-document.querySelector("#myDiv").innerHTML = beginning + liststring + ending;
