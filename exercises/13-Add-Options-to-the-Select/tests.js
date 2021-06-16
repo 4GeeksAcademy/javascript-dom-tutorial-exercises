5e6e35f90784419e73ff1b5761ce6b784c76dc36
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-06-16 18:22:04 +0000

    Issue #192 solved

--- exercises/13-Add-Options-to-the-Select/tests.js
+++ exercises/13-Add-Options-to-the-Select/tests.js
@@ -23,14 +23,6 @@ test('You should create an element ("option")', function () {
     expect(js.toString().indexOf(expected) > -1).toBeTruthy();
 });
 
-test('You should append all the countries you created, to #mySelect element', function () {
-    document.documentElement.innerHTML = html.toString();
-    const expected = 'document.querySelector("#mySelect").appendChild';
-    // we can read from the source code
-    console.log(js.toString());
-    expect(js.toString().indexOf(expected) > -1).toBeTruthy();
-});
-
 test('the js code should contain an assignment line allow you add li to list', function () {
     document.documentElement.innerHTML = html.toString();
     const expected = 'appendChild';
