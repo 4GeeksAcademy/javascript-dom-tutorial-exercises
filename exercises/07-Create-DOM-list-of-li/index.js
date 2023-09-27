d05c08f7bf8bfe1588bcccb070572a9d55ed0e9a
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-27 21:10:03 +0200

    Update index.js

--- exercises/07-Create-DOM-list-of-li/index.js
+++ exercises/07-Create-DOM-list-of-li/index.js
@@ -3,4 +3,4 @@ let listString = "";
 let ending = "</ul>";
 
 // Do not modify after this line
-document.body.innerHTML = beginning + liststring + ending;
+document.body.innerHTML = beginning + listString + ending;
