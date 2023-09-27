e054538f5ad201369682011d908d2985f8d2b7c8
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-09-27 22:17:50 +0200

    Update index.js

--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
-	
+	// Your code here
+	let newDiv = document.createElement("div");
+	newDiv.style.backgroundColor = "yellow";
+	newDiv.innerHTML = "Hello World";
+	document.body.appendChild(newDiv);
 });
